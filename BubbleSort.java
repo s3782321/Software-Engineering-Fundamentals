b14cecc27134ab046e8d300336bbf23c4cf484ee
Patrick Bui <s3782321@student.rmit.edu.au>
2020-03-10 16:49:23 +1100

    test

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void bubbleSort(int[] sortableItems){
+        int n = sortableItems.length;
+
+        for (int i=0; i<n-1; i++) {
+            for (int j=0; j<n-i-1; j++) {
+                if (sortableItems[j] > sortableItems[j+1]) {
+                    Swap(sortableItems[j],sortableItems[j+i]);
+                }
+            }
+
+        }
+    }
+    
+    
+    private void Swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        }
+    }
