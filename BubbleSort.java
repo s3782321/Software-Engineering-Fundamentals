1d1edf7f694a247b5ed6db52993de2c5adf4387a
Patrick Bui <s3782321@student.rmit.edu.au>
2020-03-10 17:37:09 +1100

    add java file

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,12 +1,10 @@
 public class BubbleSort {
 
     public static void main(String[] args) {
-
     }
 
     public void bubbleSort(int[] sortableItems){
         int n = sortableItems.length;
-
         for (int i=0; i<n-1; i++) {
             for (int j=0; j<n-i-1; j++) {
                 if (sortableItems[j] > sortableItems[j+1]) {
@@ -24,3 +22,5 @@ public class BubbleSort {
         b = temp;
         }
     }
+
+TESTINGGG
